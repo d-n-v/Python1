12450757c2999cec018b174e95a72e44c54e4c91
Dm V <soundslikeemail@yandex.ru>
2019-09-27 11:24:13 +0400

    Completed lesson 2 task 4. Normal difficulty.

--- hw02_normal.py
+++ hw02_normal.py
@@ -1,6 +1,6 @@
 import math
 import random
-
+import datetime
 
 def fill_list(list):
     length = int(input("Введите размер списка:"))
@@ -30,9 +30,9 @@ print(list_int2)
 # Ваша задача вывести дату в текстовом виде, например: второе ноября 2013 года.
 # Склонением пренебречь (2000 года, 2010 года)
 
-#date = input("Введите дату mm/dd/yyyy")
-
-
+# date = input("Введите дату дд.мм.гггг")
+#
+# datetime.strftime(date)
 
 
 
@@ -54,7 +54,27 @@ print(list_random)
 # б) элементы исходного списка, которые не имеют повторений:
 # например, lst = [1 , 2, 4, 5, 6, 2, 5, 2], нужно получить lst2 = [1, 4, 6]
 
-list_items = []
+list_items = [16, 15, 20, 20, 300, 300, 1, -7]
+list_buffer = []
+buffer_dict = {}
+list_items_nodupl = []
+list_items_unique = []
+buffer_value = 0
 
-fill_list(list_items)
+for li in list_items:
+    if li not in list_buffer:
+        list_buffer.append(li)
+list_items_nodupl = list_buffer
+print(list_items_nodupl)
 
+
+for li1 in list_items:
+    if li1 not in buffer_dict.keys():
+        buffer_dict[li1] = "1"
+    elif li1 in buffer_dict.keys():
+        buffer_dict[li1] = int(buffer_dict[li1]) + 1
+
+for li2 in buffer_dict:
+    if li2 in buffer_dict.keys() and int(buffer_dict[li2]) == 1:
+        list_items_unique.append(li2)
+print(list_items_unique)
