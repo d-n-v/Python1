6ed6b05d3b439679ac26e4e687b6c1d3d0fba98e
Dm V <soundslikeemail@yandex.ru>
2019-10-03 16:46:59 +0400

    Completed lesson 3 task 1. Normal difficulty.

--- hw03_normal.py
+++ hw03_normal.py
@@ -1,6 +1,20 @@
 from math import sqrt
 
 
+#1
+def fibonacci(n, m):
+    i = 1
+    fn = 0
+    f = 1
+
+    while i <= m:
+        if i >= n:
+            print(f)
+        f += fn
+        fn = f - fn
+        i += 1
+
+
 #2
 def sort_to_max(origin_list):
     changed = True
@@ -45,19 +59,9 @@ def par(a1, b1, c1, d1):
 # Задание-1:
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
-# def fib():
-#     a, b = 1, 1
-#     while True:
-#         yield a
-#         a, b = b, a + b
-#
-#
-# def fibonacci(n, m):
-#     for index, fibonacci_number in zip(range(n, m), fib()):
-#         return '{i:3}: {f:3}'.format(i=index, f=fibonacci_number)
-#
-#
-# print(fibonacci(1, 10))
+
+
+fibonacci(1, 7)
 
 
 # Задача-2:
@@ -66,27 +70,27 @@ def par(a1, b1, c1, d1):
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
 
-list_sorted = [2, 10, -12, 2.5, 20, -11, 4, 4, 0]
-sort_to_max(list_sorted)
-print(list_sorted)
+# list_sorted = [2, 10, -12, 2.5, 20, -11, 4, 4, 0]
+# sort_to_max(list_sorted)
+# print(list_sorted)
 
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
 
-list_to_filter = [-2, -1, 0, 1]
-res = own_filter(func, list_to_filter)
-print(res)
+# list_to_filter = [-2, -1, 0, 1]
+# res = own_filter(func, list_to_filter)
+# print(res)
 
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
 
-a_var = [1, 3]
-b_var = [4, 7]
-c_var = [2, 8]
-d_var = [-1, 4]
-
-print(par(a_var, b_var, c_var, d_var))
+# a_var = [1, 3]
+# b_var = [4, 7]
+# c_var = [2, 8]
+# d_var = [-1, 4]
+#
+# print(par(a_var, b_var, c_var, d_var))
