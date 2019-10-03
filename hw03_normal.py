8994ab5e8e8d6bb69c6e9516630bb87a84ec0ecf
Dm V <soundslikeemail@yandex.ru>
2019-10-03 15:40:54 +0400

    Completed lesson 3 task 4. Normal difficulty.

--- hw03_normal.py
+++ hw03_normal.py
@@ -1,6 +1,46 @@
 from math import sqrt
-import copy
 
+
+#2
+def sort_to_max(origin_list):
+    changed = True
+    while changed:
+        changed = False
+        for i in range(len(origin_list) - 1):
+            if origin_list[i] > origin_list[i + 1]:
+                origin_list[i], origin_list[i + 1] = origin_list[i + 1], origin_list[i]
+                changed = True
+
+
+#3
+def func(x):
+    if x > 0:
+        return True
+    else:
+        return False
+
+
+def own_filter(function, iterable):
+    for i in iterable:
+        result = []
+        if function(i):
+            result.append(i)
+    return result
+
+
+#4
+def par(a1, b1, c1, d1):
+    a = sqrt((b1[0] - a1[0]) ** 2 + (b1[1] - a1[1]) ** 2)
+
+    b = sqrt((c1[0] - d1[0]) ** 2 + (c1[1] - d1[1]) ** 2)
+
+    c = sqrt((d1[0] - a1[0]) ** 2 + (d1[1] - a1[1]) ** 2)
+
+    d = sqrt((c1[0] - b1[0]) ** 2 + (c1[1] - b1[1]) ** 2)
+    if a == b and c == d:
+        return "Это параллелограм"
+    else:
+        return "Это не параллелограм"
 
 # Задание-1:
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
@@ -26,16 +66,6 @@ import copy
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
 
-def sort_to_max(origin_list):
-    changed = True
-    while changed:
-        changed = False
-        for i in range(len(origin_list) - 1):
-            if origin_list[i] > origin_list[i + 1]:
-                origin_list[i], origin_list[i + 1] = origin_list[i + 1], origin_list[i]
-                changed = True
-
-
 list_sorted = [2, 10, -12, 2.5, 20, -11, 4, 4, 0]
 sort_to_max(list_sorted)
 print(list_sorted)
@@ -43,21 +73,6 @@ print(list_sorted)
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
-
-
-def func(x):
-    if x > 0:
-        return True
-    else:
-        return False
-
-
-def own_filter(function, iterable):
-    for i in iterable:
-        result = []
-        if function(i):
-            result.append(i)
-    return result
 
 
 list_to_filter = [-2, -1, 0, 1]
@@ -69,12 +84,9 @@ print(res)
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
 
-# a = (1, 3)
-# b = (4, 7)
-# c = (2, 8)
-# d = (-1, 4)
-#
-# ab = sqrt((a[0] - b[1]) ** 2 + (b[0] - b[1]) ** 2)
-# dc = sqrt((a[0] - 1) ** 2 + (b[0] - b[1]) ** 2)
-# ad = sqrt((a[0] - 1) ** 2 + (b[0] - b[1]) ** 2)
-# bc = sqrt((a[0] - 1) ** 2 + (b[0] - b[1]) ** 2)
+a_var = [1, 3]
+b_var = [4, 7]
+c_var = [2, 8]
+d_var = [-1, 4]
+
+print(par(a_var, b_var, c_var, d_var))
